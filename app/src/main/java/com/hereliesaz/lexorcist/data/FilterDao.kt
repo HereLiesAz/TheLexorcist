e76b7c019ecdba7f98041063f9d67f18ec32be76
Az <hereliesaz@gmail.com>
2025-08-31 00:04:21 -0500

    Refactor: Remove DAO interfaces and implementations
    This commit removes the following DAO interfaces and their implementations:
    - `AllegationDao` and `AllegationDaoImpl`
    - `CaseDao` and `CaseDaoImpl`
    - `FilterDao` and `FilterDaoImpl`
    - `GoogleSheetsCaseDao` (implementation of `CaseDao`)
    
    These files were deleted from the `com.hereliesaz.lexorcist.data` and `com.hereliesaz.lexorcist.data.remote` packages.

--- app/src/main/java/com/hereliesaz/lexorcist/data/FilterDao.kt
+++
@@ -1,9 +0,0 @@
-package com.hereliesaz.lexorcist.data
-
-import kotlinx.coroutines.flow.Flow
-
-interface FilterDao {
-    fun getFiltersForCase(spreadsheetId: String): Flow<List<Filter>>
-
-    suspend fun insert(filter: Filter)
-}
